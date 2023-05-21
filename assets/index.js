99701d9376a4fb888f09e950448fe52f57109f3a
substro <prof.aliamin@gmail.com>
2023-05-21 14:12:07 +0300

    slider addition

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,7 @@ const navToggle = document.querySelector(".mobile-nav-toggle")
 
 navToggle.addEventListener("click", () => {
   const visiblity = nav.getAttribute("data-visible")
+
   if (visiblity === "false") {
     nav.setAttribute("data-visible", true)
     navToggle.setAttribute("aria-expanded", true)
@@ -12,3 +13,31 @@ navToggle.addEventListener("click", () => {
   }
 })
 
+// slider
+let imageArray = [
+  "/assets/Images/fried-chicken.jpg",
+  "/assets/Images/fried-chicken2.jpg",
+  "/assets/Images/fried-chicken3.jpeg",
+  "/assets/Images/fried-chicken4.jpg",
+]
+
+let currentIndex = 0
+
+const nextBtn = document.querySelector("#nextBtn")
+const prevBtn = document.querySelector("#prevBtn")
+const currentImage = document.querySelector(".image_one")
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + imageArray.length) % imageArray.length
+  updateImageDisplay()
+})
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % imageArray.length
+  updateImageDisplay()
+})
+
+function updateImageDisplay() {
+  const currentImageURL = imageArray[currentIndex]
+  currentImage.src = currentImageURL
+}
