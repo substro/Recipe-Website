6808e409831f9227c9f52ea5dff748e977a56b2c
substro <prof.aliamin@gmail.com>
2023-05-26 21:48:24 +0300

    'visibility' typo

--- assets/index.js
+++ assets/index.js
@@ -4,9 +4,9 @@ const nav = document.querySelector(".primary-navigation")
 const navToggle = document.querySelector(".mobile-nav-toggle")
 
 navToggle.addEventListener("click", () => {
-  const visiblity = nav.getAttribute("data-visible")
+  const visibility = nav.getAttribute("data-visible")
 
-  if (visiblity === "false") {
+  if (visibility === "false") {
     nav.setAttribute("data-visible", true)
     navToggle.setAttribute("aria-expanded", true)
   } else {
